98dc8131806abdde93dd847c2b9fc30bc0ee4c53
Malaika-R20 <120583832+Malaika-R20@users.noreply.github.com>
2022-12-24 17:51:04 +0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a;
+	
+	printf("enter any number ");
+	scanf("%d",&a);
+	printf("cube of %d is %d",a,a*a*a);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
